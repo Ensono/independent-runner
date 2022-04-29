4cb772869621b75af276e3a4a599a0af0c936e84
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2022-04-29 09:23:43 +0100

    Used full name of the parameter

--- src/modules/AmidoBuild/command/Invoke-External.ps1
+++ src/modules/AmidoBuild/command/Invoke-External.ps1
@@ -53,7 +53,7 @@ function Invoke-External {
         if ($execute) {
 
             # Output the command being called
-            Write-Information -Message $command
+            Write-Information -MessageData $command
 
             $output = Invoke-Expression -Command $command
 
