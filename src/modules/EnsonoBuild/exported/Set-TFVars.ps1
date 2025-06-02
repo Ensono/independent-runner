694eba938eea9e4d20e308fd0086443b938abf64
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2025-06-02 14:41:46 +0100

    Downgraded version of PublishCodeCoverage task Created Alias task for Set-TFVar
    Signed by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- src/modules/EnsonoBuild/exported/Set-TFVars.ps1
+++ src/modules/EnsonoBuild/exported/Set-TFVars.ps1
@@ -3,12 +3,7 @@ function Set-TFVars() {
     <#
     
     .SYNOPSIS
-    Creates a Terraform key/variable variable string from environment variables that have a specific prefix
-
-    .DESCRIPTION
-    When working with a CI/CD system, such as Azure DevOps, the variables for Terraform are passed in as
-    environment variables. However this sometimes needs to be done several time, so this function creates
-    a valid TFVars file that can be uploaded as an artifact and used in different stages.
+    Simple wrapper cmdlet for Get-TFVars to maintain backwards compatibility
     
     .EXAMPLE
 
@@ -27,27 +22,7 @@ function Set-TFVars() {
         $prefix = "TF_VAR_*"
     )
 
-    # configure hashtable of found variables
-    $tfvars = @{}
+    Write-Warning -Message "Set-TFVars is deprecated, please use Get-TFVars instead. Set-TFVars will be removed in the next minor version."
 
-    # Output the values of the enviornment variables
-    Get-ChildItem -Path env: | Where-Object name -like $prefix | ForEach-Object {
-
-        # Get th name of the variable, without the prefix
-        $name = $_.name -replace $prefix, ""
-
-        # set the value
-        $value = $_.value
-
-        if (!($value -is [int]) -and !($value.StartsWith("{")) -and !($value.StartsWith("["))) {
-            $value = "`"{0}`"" -f $value
-        }
-
-        $tfvars[$name.ToLower()] = $value
-
-    }
-
-    foreach ($item in $tfvars.GetEnumerator()) {
-        Write-Output ("{0} = {1}" -f $item.name, $item.value)
-    }
+    Get-TFVars -prefix $prefix
 }
