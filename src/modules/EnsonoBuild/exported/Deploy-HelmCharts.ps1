39250f9877814af678e3080e3a93ca288e3f51ae
balpurewal <47520690+balpurewal@users.noreply.github.com>
2025-10-31 15:24:31 +0000

    Update src/modules/EnsonoBuild/exported/Deploy-HelmCharts.ps1
    Co-authored-by: Jack Blower <ElvenSpellmaker@users.noreply.github.com>

--- src/modules/EnsonoBuild/exported/Deploy-HelmCharts.ps1
+++ src/modules/EnsonoBuild/exported/Deploy-HelmCharts.ps1
@@ -39,7 +39,7 @@ function Deploy-HelmCharts() {
 
 		[string[]]
 		# Names of charts that should be deployed.
-		# This does not override the enabled flag, but allows a subset of enabled chartts
+		# This does not override the enabled flag, but allows a subset of enabled charts
 		# to be deployed
 		$Names,
 
