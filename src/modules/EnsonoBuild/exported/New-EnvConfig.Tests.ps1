667bedb8e3a1d115fee62a2ada24394dde762326
Bal Purewal <baldip.purewal@ensono.com>
2025-10-31 12:15:31 +0000

    Fix New-EnvConfig.Tests.ps1

--- src/modules/EnsonoBuild/exported/New-EnvConfig.Tests.ps1
+++ src/modules/EnsonoBuild/exported/New-EnvConfig.Tests.ps1
@@ -99,7 +99,7 @@ stages:
 
             New-EnvConfig -Path $stageVarFile -ScriptPath $testfolder -Cloud AWS -Stage pester
 
-            $scriptPath = [IO.Path]::Combine($testFolder, "envvar-azure-pester.ps1")
+            $scriptPath = [IO.Path]::Combine($testFolder, "envvar-aws-pester.ps1")
 
             # Check that the script has been created
             Test-Path -Path $scriptPath | Should -BeTrue
@@ -115,7 +115,7 @@ stages:
 
             New-EnvConfig -Path $stageVarFile -ScriptPath $testfolder -Cloud AWS -Stage pester
 
-            $scriptPath = [IO.Path]::Combine($testFolder, "envvar-azure-pester.sh")
+            $scriptPath = [IO.Path]::Combine($testFolder, "envvar-aws-pester.sh")
 
             # Check that the script has been created
             Test-Path -Path $scriptPath | Should -BeTrue
